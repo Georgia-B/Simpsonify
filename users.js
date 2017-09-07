54e6c276e84b2639090bb1d974bb67f8cafd0260
Georgia Barnett <Georgia-B@users.noreply.github.com>
2017-09-08 09:41:19 +1200

    Update avatar urls

--- users.js
+++ users.js
@@ -3,19 +3,19 @@ const Users = [
         firstName: 'Homer',
         lastName: 'Simpson',
         email: 'homer.simpson@springfield.com',
-        avatarUrl: 'https://goo.gl/p70v26'
+        avatarUrl: 'https://goo.gl/6LDzVy'
     },
     {
         firstName: 'Marge',
         lastName: 'Simpson',
         email: 'marge.simpson@springfield.com',
-        avatarUrl: 'https://goo.gl/xKSWD3'
+        avatarUrl: 'https://goo.gl/ULr95z'
     },
     {
         firstName: 'Lisa',
         lastName: 'Simpson',
         email: 'lisa.simpson@springfield.com',
-        avatarUrl: 'https://goo.gl/cw863l'
+        avatarUrl: 'https://goo.gl/PcHuo1'
     },
     {
         firstName: 'Bart',
@@ -27,145 +27,145 @@ const Users = [
 	firstName: 'Ned',
 	lastName: 'Flanders',
 	email: 'ned.flanders@springfield.com',
-	avatarUrl: 'https://goo.gl/KDpYYM'
+	avatarUrl: 'https://goo.gl/dbgrn8'
     },
     {
-	firstName: 'Mod',
+	firstName: 'Maude',
 	lastName: 'Flanders',
-	email: 'mod.flanders@springfield.com',
-	avatarUrl: 'https://goo.gl/WVp79q'
+	email: 'maude.flanders@springfield.com',
+	avatarUrl: 'https://goo.gl/sEHHra'
     },
     {
 	firstName: 'Todd',
 	lastName: 'Flanders',
 	email: 'todd.flanders@springfield.com',
-	avatarUrl: 'https://goo.gl/DXfbuX'
+	avatarUrl: 'https://goo.gl/XUUFkF'
     },
     {
 	firstName: 'Rodd',
 	lastName: 'Flanders',
 	email: 'rodd.flanders@springfield.com',
-	avatarUrl: 'https://goo.gl/l6zd71'
+	avatarUrl: 'https://goo.gl/hedzCG'
     },
     {
 	firstName: 'Edna',
 	lastName: 'Krabappel',
 	email: 'edna.krabappel@springfield.com',
-	avatarUrl: 'https://goo.gl/0oU00B'
+	avatarUrl: 'https://goo.gl/XPgHh8'
     },
     {
 	firstName: 'Seymour',
 	lastName: 'Skinner',
 	email: 'seymour.skinner@springfield.com',
-	avatarUrl: 'https://goo.gl/eeWYX3'
+	avatarUrl: 'https://goo.gl/VwWdd7'
     },
     {
 	firstName: 'Montgomery',
 	lastName: 'Burns',
 	email: 'montgomery.burns@springfield.com',
-	avatarUrl: 'https://goo.gl/mXfHMU'
+	avatarUrl: 'https://goo.gl/GSvFX9'
     },
     {
 	firstName: 'Waylon',
 	lastName: 'Smithers',
 	email: 'waylon.smithers@springfield.com',
-	avatarUrl: 'https://goo.gl/2VsGvN'
+	avatarUrl: 'https://goo.gl/8zbBua'
     },
     {
 	firstName: 'Apu',
 	lastName: 'Nahasapeemapetilon',
 	email: 'apu.nahasapeemapetilon@springfield.com',
-	avatarUrl: 'https://goo.gl/YYo6le'
+	avatarUrl: 'https://goo.gl/P9nb79'
     },
     {
 	firstName: 'Moe',
 	lastName: 'Szyslak',
 	email: 'moe.szyslak@springfield.com',
-	avatarUrl: 'https://goo.gl/J8eE7y'
+	avatarUrl: 'https://goo.gl/f8Bmfz'
     },
     {
 	firstName: 'Barney',
 	lastName: 'Gumble',
 	email: 'barney.gumble@springfield.com',
-	avatarUrl: 'https://goo.gl/f5eYBI'
+	avatarUrl: 'https://goo.gl/g4Smvz'
     },
     {
 	firstName: 'Comic Book',
 	lastName: 'Guy',
 	email: 'comicbook.guy@springfield.com',
-	avatarUrl: 'https://goo.gl/HYBShs'
+	avatarUrl: 'https://goo.gl/sWJVqf'
     },
     {
 	firstName: 'Patty',
 	lastName: 'Bouvier',
 	email: 'patty.bouvier@springfield.com',
-	avatarUrl: 'https://goo.gl/Ema7xr'
+	avatarUrl: 'https://goo.gl/Rvz7Ed'
     },
     {
 	firstName: 'Carl',
 	lastName: 'Carlson',
 	email: 'carl.carlson@springfield.com',
-	avatarUrl: 'https://goo.gl/DrgZtD'
+	avatarUrl: 'https://goo.gl/DD5Fav'
     },
     {
 	firstName: 'Troy',
 	lastName: 'McClure',
 	email: 'troy.mcclure@springfield.com',
-	avatarUrl: 'https://goo.gl/oHeBCW'
+	avatarUrl: 'https://goo.gl/MfapvX'
     },
     {
 	firstName: 'Kirk',
 	lastName: 'Van Houten',
 	email: 'kirk.van.houten@springfield.com',
-	avatarUrl: 'https://goo.gl/hJHxpx'
+	avatarUrl: 'https://goo.gl/wYtdWd'
     },
     {
 	firstName: 'Milhouse',
 	lastName: 'Van Houten',
 	email: 'milhouse.vanhouten@springfield.com',
-	avatarUrl: 'https://goo.gl/K7PPEz'
+	avatarUrl: 'https://goo.gl/zgV6vh'
     },
     {
 	firstName: 'Disco',
 	lastName: 'Stu',
 	email: 'disco.stu@springfield.com',
-	avatarUrl: 'https://goo.gl/mSEKbc'
+	avatarUrl: 'https://goo.gl/wY6Rga'
     },
     {
 	firstName: 'Lenny',
 	lastName: 'Leonard',
 	email: 'lenny.leonard@springfield.com',
-	avatarUrl: 'https://goo.gl/NHhhWy'
+	avatarUrl: 'https://goo.gl/Jv8hhG'
     },
     {
 	firstName: 'Kent',
 	lastName: 'Brockman',
 	email: 'kent.brockman@springfield.com',
-	avatarUrl: 'https://goo.gl/SzaSG7'
+	avatarUrl: 'https://goo.gl/5tqMAH'
     },
     {
 	firstName: 'Helen',
 	lastName: 'Lovejoy',
 	email: 'helen.lovejoy@springfield.com',
-	avatarUrl: 'https://goo.gl/iQRx87'
+	avatarUrl: 'https://goo.gl/jRvJjq'
     },
     {
 	firstName: 'Agnes',
 	lastName: 'Skinner',
 	email: 'agnes.skinner@springfield.com',
-	avatarUrl: 'https://goo.gl/Bh1983'
+	avatarUrl: 'https://goo.gl/pN2wWg'
     },
     {
 	firstName: 'Sideshow',
 	lastName: 'Bob',
 	email: 'sideshow.bob@springfield.com',
-	avatarUrl: 'https://goo.gl/dHti8U'
+	avatarUrl: 'https://goo.gl/8Af6Rf'
     },
     {
 	firstName: 'Hans',
 	lastName: 'Moleman',
 	email: 'hans.moleman@springfield.com',
-	avatarUrl: 'https://goo.gl/YmQ18P'
+	avatarUrl: 'https://goo.gl/8yWdvQ'
     }
 ];
 
