64f03a9a2c1b6562754118acc639339ef35f58bc
Georgia Barnett <Georgia-B@users.noreply.github.com>
2017-09-07 16:09:04 +1200

    Update users.js with more Simpsons

--- users.js
+++ users.js
@@ -22,7 +22,103 @@ const Users = [
         lastName: 'Simpson',
         email: 'bart.simpson@springfield.com',
         avatarUrl: './res/bart.png'
-    }
+    },
+    {
+		firstName: 'Ned',
+		lastName: 'Flanders',
+		email: 'ned.flanders@springfield.com',
+		avatarUrl: 'https://goo.gl/KDpYYM'
+	},
+	{
+		firstName: 'Mod',
+		lastName: 'Flanders',
+		email: 'mod.flanders@springfield.com',
+		avatarUrl: 'https://goo.gl/WVp79q'
+	},
+	{
+		firstName: 'Todd',
+		lastName: 'Flanders',
+		email: 'todd.flanders@springfield.com',
+		avatarUrl: 'https://goo.gl/DXfbuX'
+	},
+	{
+		firstName: 'Rodd',
+		lastName: 'Flanders',
+		email: 'rodd.flanders@springfield.com',
+		avatarUrl: 'https://goo.gl/l6zd71'
+	},
+	{
+		firstName: 'Edna',
+		lastName: 'Krabappel',
+		email: 'edna.krabappel@springfield.com',
+		avatarUrl: 'https://goo.gl/0oU00B'
+	},
+	{
+		firstName: 'Seymour',
+		lastName: 'Skinner',
+		email: 'seymour.skinner@springfield.com',
+		avatarUrl: 'https://goo.gl/eeWYX3'
+	},
+	{
+		firstName: 'Montgomery',
+		lastName: 'Burns',
+		email: 'montgomery.burns@springfield.com',
+		avatarUrl: 'https://goo.gl/mXfHMU'
+	},
+	{
+		firstName: 'Waylon',
+		lastName: 'Smithers',
+		email: 'waylon.smithers@springfield.com',
+		avatarUrl: 'https://goo.gl/2VsGvN'
+	},
+	{
+		firstName: 'Apu',
+		lastName: 'Nahasapeemapetilon',
+		email: 'apu.nahasapeemapetilon@springfield.com',
+		avatarUrl: 'https://goo.gl/YYo6le'
+	},
+	{
+		firstName: 'Moe',
+		lastName: 'Szyslak',
+		email: 'moe.szyslak@springfield.com',
+		avatarUrl: 'https://goo.gl/J8eE7y'
+	},
+	{
+		firstName: 'Barney',
+		lastName: 'Gumble',
+		email: 'barney.gumble@springfield.com',
+		avatarUrl: 'https://goo.gl/f5eYBI'
+	},
+	{
+		firstName: 'Comic Book',
+		lastName: 'Guy',
+		email: 'comicbook.guy@springfield.com',
+		avatarUrl: 'https://goo.gl/HYBShs'
+	},
+	{
+		firstName: 'Patty',
+		lastName: 'Bouvier',
+		email: 'patty.bouvier@springfield.com',
+		avatarUrl: 'https://goo.gl/Ema7xr'
+	},
+	{
+		firstName: 'Carl',
+		lastName: 'Carlson',
+		email: 'carl.carlson@springfield.com',
+		avatarUrl: 'https://goo.gl/DrgZtD'
+	},
+	{
+		firstName: 'Troy',
+		lastName: 'McClure',
+		email: 'troy.mcclure@springfield.com',
+		avatarUrl: 'https://goo.gl/oHeBCW'
+	},
+	{
+		firstName: 'Kirk',
+		lastName: 'Van Houten',
+		email: 'kirk.van.houten@springfield.com',
+		avatarUrl: 'https://goo.gl/hJHxpx'
+	}
 ];
 
 module.exports = Users;
